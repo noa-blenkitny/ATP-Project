2e726596dc6b75ea460faa4bb083209234ec760c
noa-blenkitny <noa.blenkitny@gmail.com>
2021-06-06 13:45:53 +0300

    added classes from last project

---
+++ src/View/IView.java
@@ -0,0 +1,11 @@
+package View;
+
+public interface IView {
+    void save();
+    void load();
+    void New();
+    void switchScene();
+    void help();
+    void about();
+    void Proporties();
+}
