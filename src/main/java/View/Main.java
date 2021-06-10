50322bced1dc35ddf6c83f145e22f031c2433c7b
noa-blenkitny <noa.blenkitny@gmail.com>
2021-06-10 16:46:58 +0300

    added log4j2

---
+++ src/main/java/View/Main.java
@@ -0,0 +1,66 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package View;
+
+import Model.IModel;
+import Model.MyModel;
+import ViewModel.MyViewModel;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
+
+public class Main extends Application {
+    public Main() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getClassLoader().getResource("MyView.fxml"));
+        Parent root = (Parent)fxmlLoader.load();
+       // Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        primaryStage.setTitle("Jungle Adventure");
+        primaryStage.setScene(new Scene(root, 1000.0D, 650.0D));
+        primaryStage.show();
+        IModel model = new MyModel();
+        MyViewModel myViewModel = new MyViewModel(model);
+        MyViewController myViewController = (MyViewController)fxmlLoader.getController();
+        myViewController.setViewModel(myViewModel);
+        myViewController.setPrimaryStage(primaryStage);
+        SetStageCloseEvent(primaryStage, model);
+
+    }
+
+    public void SetStageCloseEvent(Stage stage, IModel model){
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to exit?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    model.stopServers();
+                    Platform.exit();
+                    System.exit(0);
+                }
+                else{
+                    event.consume();
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/View/choosePlayerController.java
@@ -0,0 +1,69 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.util.Observable;
+
+public class choosePlayerController extends AView{
+
+
+    public static String chosenPlayer;
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+    public void changePlayer(String player)
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../resources/MazeWindow.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            mazeWindowController newView = fxmlLoader.getController();
+            newView.choosePlayer(player);
+            chosenPlayer = player;
+        }
+        catch (Exception e)
+        {}
+    }
+
+    public static String getChosenPlayer() {
+        return chosenPlayer;
+    }
+
+    public void women(ActionEvent actionEvent) {
+        chosenPlayer = "women";
+        switchScene("chooseHardness.fxml",getStage());
+      // changePlayer("women");
+    }
+
+    public void monkey(ActionEvent actionEvent) {
+        chosenPlayer = "monkey";
+        switchScene("chooseHardness.fxml",getStage());
+        //changePlayer("monkey");
+    }
+
+    public void fox(ActionEvent actionEvent) {
+        chosenPlayer = "fox";
+        switchScene("chooseHardness.fxml",getStage());
+        //changePlayer("fox");
+    }
+
+    public void gazelle(ActionEvent actionEvent) {
+        chosenPlayer = "gazelle";
+        switchScene("chooseHardness.fxml",getStage());
+        //changePlayer("gazelle");
+    }
+
+    public void child(ActionEvent actionEvent) {
+        chosenPlayer = "child";
+        switchScene("chooseHardness.fxml",getStage());
+        //changePlayer("child");
+    }
+
+    public void man(ActionEvent actionEvent) {
+        chosenPlayer = "man";
+        switchScene("chooseHardness.fxml",getStage());
+       // changePlayer("man");
+    }
+}
